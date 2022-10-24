92dfac88618296c0250cf89d50d1c96126f4b06f
katyasymchuk <katyasokaal@gmail.com>
2022-10-24 14:01:37 +0300

    react project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {setupStore} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
+
+import './index.css';
+import {setupStore} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = setupStore();
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-        <App />
+            <App/>
         </BrowserRouter>
     </Provider>
 );
